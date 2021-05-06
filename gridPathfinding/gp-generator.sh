6eacc5f80cd804fd8e0284d1f334fdb84d546157
Tianyi Gu <2008tygu@gmail.com>
2021-05-05 21:03:40 -0400

    grid pathfinding - create large instance 100x1000 map

--- gridPathfinding/gp-generator.sh
+++ gridPathfinding/gp-generator.sh
@@ -2,6 +2,6 @@
 
 for (( i=1; i <= 100; i++ )); do
     #python make-gp.py -s $i > "$i.gp"; 
-    #python make-gp.py -t startObstacle -s $i > "$i.gp"; 
-    python make-gp.py -t uniform -s $i > "$i.gp"; 
+    python make-gp.py -t startObstacle -s $i > "$i.gp"; 
+    #python make-gp.py -t uniform -s $i > "$i.gp"; 
 done
