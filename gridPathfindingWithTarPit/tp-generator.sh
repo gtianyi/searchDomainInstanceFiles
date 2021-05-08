85d150c806a88ac57dc89259482e57bd6213ab11
Tianyi Gu <2008tygu@gmail.com>
2021-05-08 17:26:53 -0400

    grid pathfinding with tar pit - update

--- gridPathfindingWithTarPit/tp-generator.sh
+++ gridPathfindingWithTarPit/tp-generator.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+#mkdir -p startObstacle_large
+#for (( i=1; i <= 100; i++ )); do
+    #python make-tp.py -r 100 -c 1000 -pd 0.2 -t startObstacle -s $i > ./startObstacle_large/"$i.tp"; 
+#done
+
+#mkdir -p goalObstacle_large
+#for (( i=1; i <= 100; i++ )); do
+    #python make-tp.py -r 100 -c 1000 -pd 0.2 -t goalObstacle -s $i > ./goalObstacle_large/"$i.tp"; 
+#done
+
+#mkdir -p uniformObstacle_large
+#for (( i=1; i <= 100; i++ )); do
+    #python make-tp.py -r 100 -c 1000 -pd 0.2 -t uniform -s $i > ./uniformObstacle_large/"$i.tp"; 
+#done
+
+mkdir -p startObstacle
 for (( i=1; i <= 100; i++ )); do
-    #python make-gp.py -s $i > "$i.gp"; 
-    python make-gp.py -t startObstacle -s $i > "$i.gp"; 
-    #python make-gp.py -t uniform -s $i > "$i.gp"; 
+    python make-tp.py -t startObstacle -s $i > ./startObstacle/"$i.tp"; 
 done
+
+mkdir -p goalObstacle
+for (( i=1; i <= 100; i++ )); do
+    python make-tp.py -t goalObstacle -s $i > ./goalObstacle/"$i.tp"; 
+done
+
+mkdir -p uniformObstacle
+for (( i=1; i <= 100; i++ )); do
+    python make-tp.py -t uniform -s $i > ./uniformObstacle/"$i.tp"; 
+done
+
+
